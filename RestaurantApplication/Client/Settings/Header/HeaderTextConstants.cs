8ac7d6ec7f682eb0956ecbc0194838bc12e80b25
Alex <contact@alexdecesare.com>
2022-04-11 09:50:05 -0400

    Added login buttons
    Added buttons for the login page

--- RestaurantApplication/Client/Settings/Header/HeaderTextConstants.cs
+++ RestaurantApplication/Client/Settings/Header/HeaderTextConstants.cs
@@ -10,14 +10,6 @@ namespace RestaurantApplication.Client.Settings.Header
         /// <summary>
         /// The title for the login page header
         /// </summary>
-        public static string WelcomeTitle = "Welcome";
-        /// <summary>
-        /// The content for the login page header
-        /// </summary>
-        public static string WelcomeContent = $"Welcome to {AppConstants.Name}";
-        /// <summary>
-        /// The title for the login page header
-        /// </summary>
         public static string LoginTitle = "Login";
         /// <summary>
         /// The content for the login page header
