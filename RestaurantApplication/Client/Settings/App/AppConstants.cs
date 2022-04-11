36bffa2351eb75549466e3d7100b9bdb8d3b7ed9
Alex <contact@alexdecesare.com>
2022-04-10 21:40:21 -0400

    Added login page
    Added the login page

---
+++ RestaurantApplication/Client/Settings/App/AppConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantApplication.Client.Settings.App
+{
+    /// <summary>
+    /// The constants for the application
+    /// </summary>
+    public class AppConstants
+    {
+        /// <summary>
+        /// The constant for the app name
+        /// </summary>
+        public static string Name = "Restaurant Manager";
+    }
+}
---
+++ RestaurantApplication/Client/Settings/Header/HeaderTextConstants.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantApplication.Client.Settings.App;
+
+namespace RestaurantApplication.Client.Settings.Header
+{
+    /// <summary>
+    /// Holds the text for the header
+    /// </summary>
+    public static class HeaderTextConstants
+    {
+        /// <summary>
+        /// The title for the login page header
+        /// </summary>
+        public static string WelcomeTitle = "Welcome";
+        /// <summary>
+        /// The content for the login page header
+        /// </summary>
+        public static string WelcomeContent = $"Welcome to {AppConstants.Name}";
+        /// <summary>
+        /// The title for the login page header
+        /// </summary>
+        public static string LoginTitle = "Login";
+        /// <summary>
+        /// The content for the login page header
+        /// </summary>
+        public static string LoginContent = $"Login to {AppConstants.Name}";
+    }
+}
